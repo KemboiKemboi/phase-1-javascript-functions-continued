1fca4802daeff24c21aabf0090dfc17b0c2e5be6
Felix Kemboi <felixkemboi824@gmail.com>
2025-03-24 11:28:06 +0300

    initiate commit

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+// 1. Saturday Fun function
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // 2. Monday Work function
+  function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // 3. Wrap Adjective function
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Example usage
+  console.log(saturdayFun()); // "This Saturday, I want to roller-skate!"
+  console.log(saturdayFun("hike")); // "This Saturday, I want to hike!"
+  
+  console.log(mondayWork()); // "This Monday, I will go to the office."
+  console.log(mondayWork("work from home")); // "This Monday, I will work from home."
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("a dedicated programmer")); // "You are !!!a dedicated programmer!!!"
+  
